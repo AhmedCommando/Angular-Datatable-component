663b66848c1accdd1267ef70684f1ee70288e0d2
ahmedkb <a.bejaoui@energon.io>
2018-01-18 14:22:50 +0100

    Add Delete and View action event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  public data = [{
-    x: 'xx',
-    y: 'yy'
-  },
-  {
-    x: 'zzz',
-    y: 'hhh'  
-  },
-  {
-    x: 'zzggz',
-    y: 'hhh'  
-  },
-  {
-    x: 'zzssz',
-    y: 'hhh'  
-  },
-  {
-    x: 'zzz',
-    y: 'hhh'
-  }
-  ]
+  public data = [
+      {
+        col1: 'L1',
+        col2: 'L1',
+        col3: 'L1'
+      },
+      {
+        col1: 'L2',
+        col2: 'L2',
+        col3: 'L2'
+      },
+      {
+        col1: 'L3',
+        col2: 'L3',
+        col3: 'L3'
+      },
+      {
+        col1: 'L4',
+        col2: 'L4',
+        col3: 'L4'
+      }
+  ];
 
   public actions = [
-    { edit: "<button type='button'>Edit</button>"},
-    { delete: "<button type='button'>Delete</button>"}
-  ]
+    { edit: '<button type="button">Edit</button>'},
+    { delete: '<button type="button">Delete</button>'}
+  ];
 
   onEdit(row) {
-    console.log(row)
+    console.log('Edit', row);
+  }
+
+  onDelete(row) {
+    console.log('Delete', row);
   }
 }
--- src/app/data-table-component/data-table-component.component.ts
+++ src/app/data-table-component/data-table-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import * as $ from 'jquery';
-import 'datatables.net'
+import 'datatables.net';
 @Component({
   selector: 'app-datatableComponent',
   templateUrl: './data-table-component.component.html',
@@ -11,9 +11,13 @@ export class DataTableComponentComponent implements AfterViewInit, OnInit {
   @Input() edit: boolean;
   @Input() delete: boolean;
   @Input() view: boolean;
+  // tslint:disable-next-line:no-output-on-prefix
   @Output() onEdit: EventEmitter<any> = new EventEmitter<any>();
-  
-  
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onDelete: EventEmitter<any> = new EventEmitter<any>();
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onView: EventEmitter<any> = new EventEmitter<any>();
+
   public headers = [];
   public tableData = [];
   public tableActions = [];
@@ -22,62 +26,69 @@ export class DataTableComponentComponent implements AfterViewInit, OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.getHeaders()
-    this.getActions()
+    this.getHeaders();
+    this.getActions();
   }
 
   ngAfterViewInit() {
-    this.test()
+    this.StartDataTable();
   }
 
-
-  initDatatable() {
-    let exampleId: any = $('#example');
-    this.tableWidget = exampleId.DataTable({
+  StartDataTable() {
+    const table: any = $('#example');
+    this.tableWidget = table.DataTable({
       select: true
     });
-    
   }
 
   getHeaders() {
     const data = this.data;
     let tableData;
-    data.forEach((obj,index) => {
-      tableData = []
-      for (let key in obj) {
+    data.forEach((obj, index) => {
+      tableData = [];
+      for (const key of Object.keys(obj)) {
         if (this.headers.indexOf(key) === -1) {
-          this.headers.push(key)
+          this.headers.push(key);
         }
-        tableData.push(obj[key])
+        tableData.push(obj[key]);
       }
-      this.tableData.push(tableData)
-    })
+      this.tableData.push(tableData);
+    });
   }
 
-
+  /**
+   * Add action buttons to table
+   */
   getActions() {
-    if(this.edit) this.headers.push("Edit")
-    if(this.delete) this.headers.push("Delete")
-    if(this.view) this.headers.push("View")
+    if (this.edit) {
+      this.headers.push('Edit');
+    }
+    if (this.delete) {
+      this.headers.push('Delete');
+    }
+    if (this.view) {
+      this.headers.push('View');
+    }
   }
 
+  /**
+   * Delete event
+   * @param row
+   */
   deleteClick(row) {
-    console.log(row)
+    this.onDelete.emit(row);
   }
 
   viewClick(row) {
-
+    this.onView.emit(row);
   }
 
+  /**
+   * Edit event
+   * @param row
+   */
   editClick(row) {
-    console.log(row)
-    this.onEdit.emit(row)
-  }
-
-  test () {
-    
-    //this.getTableData();
-    this.initDatatable()
+    this.onEdit.emit(row);
   }
 
 }
